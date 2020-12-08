00d63e926811c1356a55bb4426c003daf398782b
skumar6 <skumar6@empoweredbenefits.com>
2020-12-08 14:21:54 +0530

    issue fixed

--- src/app.js
+++ src/app.js
@@ -7,16 +7,10 @@ const app = express();
 app.set('views',path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', function(req, res) {
-    res.render('index', { 
-        title: 'Index'
-    });
-});
+app.get('/', (req, res) => res.render('index', {title: 'Index'}));
 
 
-app.listen(3000, function(){
-    console.log("PS Project Running on port 3000!") 
-}) 
+app.listen(3000, () => console.log("PS Project Running on port 3000!"));

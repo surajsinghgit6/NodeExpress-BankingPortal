36407a65e2492fabff99e13c57db6a7d3668d25b
skumar6 <skumar6@empoweredbenefits.com>
2020-12-17 12:48:49 +0530

    File handling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
-const express = require("express");
 
+const express = require("express");
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -18,5 +18,11 @@ const userData = fs.readFileSync("src/json/users.json", { encoding: "utf8" });
 const users = JSON.parse(userData);
 
 app.get("/", (req, res) => res.render("index", { title: "Account Summary", accounts: accounts }));
+
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"));

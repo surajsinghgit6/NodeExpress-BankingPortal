4f51dec7b667b6623f6c70f2cce84bed4a922fb3
skumar6 <skumar6@empoweredbenefits.com>
2020-12-17 12:30:32 +0530

    Read Account data

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 
 const app = express();
 
-app.set('views',path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+const accountData = fs.readFileSync("src/json/accounts.json", {
+  encoding: "utf8",
+});
+const accounts = JSON.parse(accountData);
 
-app.get('/', (req, res) => res.render('index', {title: 'Index'}));
+const userData = fs.readFileSync("src/json/users.json", { encoding: "utf8" });
+const users = JSON.parse(userData);
 
+app.get("/", (req, res) => res.render("index", { title: "Account Summary", accounts: accounts }));
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"));
